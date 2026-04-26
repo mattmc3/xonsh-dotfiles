b9c2023faa4bac66a38c0e587aa7191656267bc2
mattmc3 <mattmc3@gmail.com>
2026-04-26 11:39:51 -0400

    Fix up() to use os.chdir

--- rc.d/config/utils.py
+++ rc.d/config/utils.py
@@ -1,7 +1,9 @@
+import os
 from xonsh.built_ins import XSH
 
 def up_(args):
     """Go up any number of directories"""
     n = int(args[0]) if args else 1
-    XSH.execer.exec(f"cd {'../' * n}")
+    os.chdir('../' * n)
 XSH.aliases['up'] = up_
+

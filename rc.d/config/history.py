f804f75306197a1292855788ca85f163085a75a1
mattmc3 <mattmc3@gmail.com>
2026-05-08 09:50:11 -0400

    Ignore commands with a leading space

--- rc.d/config/history.py
+++ rc.d/config/history.py
@@ -3,6 +3,9 @@ from xonsh.built_ins import XSH
 # History backend
 XSH.env["XONSH_HISTORY_BACKEND"] = "sqlite"
 
+# Ignore commands beginning with a space.
+XSH.env["HISTCONTROL"] = "ignorespace"
+
 # Optional: explicit history file (XDG-clean)
 XSH.env["XONSH_HISTORY_FILE"] = (
     __import__("pathlib").Path("~/.local/share/xonsh/xonsh-history.db").expanduser()
